1f2cb9a79731db7118b2267437e74f8a9b434f9c
sora <147121795+sora7716@users.noreply.github.com>
2024-05-10 15:04:34 +0900

    listの代入、追加

--- PG3_03_01/main.cpp
+++ PG3_03_01/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-#include <cstdio>
+#include <list>
+using namespace std;
 
 int main() {
+
+	list<int>lst{ 1,2,3 };
+	auto itr = lst.begin();//0番目の要素を指すイテレータ
+	cout << *itr<<"\n";//itrは0番目の要素を指しているので1を表示
+	*itr = 9;//0番目の要素を9に変更
+	cout << *itr << "\n";//itrは0番目の要素を指しているので9を表示
+	itr++;
+	*itr = 10;//1番目の要素を10に変更
+	cout << *itr << "\n" << "\n";//itrは1番目の要素を指しているので10を表示
+	lst.push_front(114);//前に要素を追加
+	
+	lst.push_back(225);//後ろに要素を追加
+
+	//要素をすべて表示
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		cout << *itr << "\n";
+	}
 
 	return 0;
 }
