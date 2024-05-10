abc7441d408195567940094be3ee8d34181f2875
sora <147121795+sora7716@users.noreply.github.com>
2024-05-10 15:42:18 +0900

    最初の値の削除、最後の値の削除、任意の場所の値の削除

--- PG3_03_01/main.cpp
+++ PG3_03_01/main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <list>
+#include <string>
 using namespace std;
 
+void Printf(list <int> list,string label) {
+	cout << label<<"\n";//labelの文字を表示
+	for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr) {
+		cout << *itr<<",";//listをすべて表示
+	}
+	cout << "\n";//改行
+}
+
 int main() {
-	list<int>list{ 1,3,5,7,10 };
 
-	//最初のリスト
-	cout << "追加前" << "\n";
-	for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr) {
-		cout << *itr << "\n";
-	}
-	cout << "==================================" << "\n";
-	//任意の場所に値を追加
-	for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr) {
-		
-		if (*itr == 3) {
-			itr = list.insert(itr,0);
-			++itr;
-        }
-	}
-	cout << "追加後" << "\n";
-	//追加後のリスト
-	for (std::list<int>::iterator itr = list.begin(); itr != list.end(); ++itr) {
-		cout << *itr << "\n";
-	}
-
+	list<int> lst{ 3,1,4,1,5 };//リストの宣言
+	Printf(lst,"削除前");
+	lst.pop_front();//最初の値を削除
+	Printf(lst, "最初の値を削除");
+	lst.pop_back();//最後の値を削除
+	Printf(lst, "最後の値を削除");
+	
+	cout << "==================" << "\n";
+	lst = { 1,2,3,4,5,6,7,8,9,0 };
+	Printf(lst, "新しく代入");
+	auto itr = lst.begin();
+	++itr;
+	++itr;
+	lst.erase(itr);//itr番目を削除
+	Printf(lst, "itr番目を削除");
 	return 0;
 }
