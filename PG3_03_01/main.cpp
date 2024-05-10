47585c3e489c2d486746162104ec1739e948b2a1
sora <147121795+sora7716@users.noreply.github.com>
2024-05-10 14:23:38 +0900

    テンプレートの作成

--- PG3_03_01/main.cpp
+++ PG3_03_01/main.cpp
@@ -1,18 +1,7 @@
+#include <iostream>
 #include <cstdio>
-#include<iostream>
-#include <vector>
-#include <string>
-using namespace std;
 
 int main() {
-	
-	vector<string> f = { "ローソン","ファミマ","セブン" };
-	for (vector<string>::iterator it_f = f.begin(); it_f != f.end(); it_f++) {
-
-		cout << *it_f << endl;
-
-	}
-
 
 	return 0;
 }
